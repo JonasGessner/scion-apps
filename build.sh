266c1d8592b633f7af7a120345a804e3385b2e4e
Jonas Gessner <JonasGessner@users.noreply.github.com>
2021-03-12 21:49:37 +0100

    Add documentation about gomobile version

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 # Deal with go stuff...
 go mod download golang.org/x/mobile
 
+# To support macOS, use this fork of gomobile: https://github.com/ydnar/gomobile/tree/support-catalyst Download code, go run ./cmd/gobind and go run ./cmd/gomobile, then gomobile init
 # Build
 GO11MODULE=on gomobile bind -target=ios ./pkg/appnet
 
